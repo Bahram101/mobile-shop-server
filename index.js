366ba648a565640ef399b173aee624c11e9e2e52
Bahrom <bahram101@mail.ru>
2024-06-10 15:06:32 +0500

    mongo

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
-import productRoutes from './routes/productRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import errorMiddleware from "./utils/errorMiddleware.js"; 
+import productRoutes from "./routes/productRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import errorMiddleware from "./utils/errorMiddleware.js";
 
 const app = express();
 dotenv.config();
@@ -18,16 +18,21 @@ app.use(cors());
 app.use(express.json());
 
 //routes
-app.use("/api/auth", userRoutes); 
-app.use("/api/product", productRoutes)
-app.use("/api/category", categoryRoutes)
-app.use(errorMiddleware)
+app.use("/api/auth", userRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/category", categoryRoutes);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL).then(() => {
-      console.log("DB is ok");
-    });
+    await mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("DB is ok");
+      });
     app.listen(PORT, () => console.log("Server is running!"));
   } catch (err) {
     console.log("error2", err);
